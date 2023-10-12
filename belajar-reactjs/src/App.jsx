99d6596e55c56a6f307764030e1ceccfc34d159a
Mumtazrullah <anwarullahmumtaz404@gmail.com>
2023-10-12 20:31:19 +0700

    tambah file

---
+++ belajar-reactjs/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Example from './coba';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <h1>Belajar ReactJS</h1>
+      <Example />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ belajar-reactjs/src/coba.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Example() {
+  return (
+    <div>
+      <p>Ini adalah contoh komponen yang terhubung dengan App.</p>
+    </div>
+  );
+}
+
+export default Example;
